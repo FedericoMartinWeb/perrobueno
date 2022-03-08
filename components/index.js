cf019a7a1a0ac790970a62de68c38d64cda2dfe3
Federico Palacios <FedericoPalacios@bitbucket.org>
2022-03-08 12:40:50 -0300

    starting

---
+++ components/index.js
@@ -0,0 +1,19 @@
+// import Navbar from "./Navbar";
+// import Footer from "./Footer";
+// import Aside from "./Aside";
+// import SideTitle from "./SideTitle";
+// import Boxes from "./Boxes";
+// import BoxItem from "./BoxItem";
+// import SubTitle from "./SubTitle";
+// import PopUp from "./PopUp";
+
+// export {
+//     Navbar,
+//     Footer,
+//     Aside,
+//     SideTitle,
+//     Boxes,
+//     BoxItem,
+//     SubTitle,
+//     PopUp
+// };
---
+++ constants/images.js
@@ -0,0 +1,37 @@
+import educacion3 from '../assets/images/educacion3.jpg'
+import guarderiatop from '../assets/images/guarderiatop.jpg'
+import happyface from '../assets/images/happyface.png'
+import happyfaceblack from '../assets/images/happyfaceblack.png'
+import logoblanco from '../assets/images/logoblanco.png'
+import logorojo from '../assets/images/logorojo.png'
+import pb_logo from '../assets/images/pb_logo.png'
+import perro_3 from '../assets/images/perro_3.jpg'
+import perro_5 from '../assets/images/perro_5.jpg'
+import perro_6 from '../assets/images/perro_6.jpg'
+import perro_prueba from '../assets/images/perro_prueba.jpg'
+import perrobuenofoto from '../assets/images/perrobuenofoto.jpg'
+import prueba from '../assets/images/prueba.jpg'
+import teaming from '../assets/images/teaming.png'
+import teaming2 from '../assets/images/teaming2.png'
+import educacion from '../assets/images/educacion.jpg'
+import educacion2 from '../assets/images/educacion2.jpg'
+
+export default {
+    educacion3,
+    guarderiatop,
+    happyface,
+    happyfaceblack,
+    logoblanco,
+    logorojo,
+    pb_logo,
+    perro_3,
+    perro_5,
+    perro_6,
+    perro_prueba,
+    perrobuenofoto,
+    prueba,
+    teaming,
+    teaming2,
+    educacion,
+    educacion2
+}
