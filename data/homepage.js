5b49c7bb8b8660741210b20a3b27e34c1fdba980
Federico Palacios <federicompalacios@hotmail.com>
2022-10-21 10:16:52 -0300

    elmino imagen

--- data/homepage.js
+++ data/homepage.js
@@ -1,136 +1,148 @@
 export const homepage = {
-    "data": {
-        "id": 1,
-        "attributes": {
-            "hero": {
-                "id": 1,
-                "title": "Bienvenido a PERROBUENO Madrid",
-                "subtitle": "¡Tu perro al cole!",
-                "image": "newherohome"
+    data: {
+        id: 1,
+        attributes: {
+            hero: {
+                id: 1,
+                title: "Bienvenido a PERROBUENO Madrid",
+                subtitle: "¡Tu perro al cole!",
+                image: "newherohome",
             },
-            "services": {
-                "id": 1,
-                "title": "Servicios",
-                "descriptionFirst": "PerroBueno fue creado por amantes de los animales para darles un servicio personalizado. Es un lugar especial para que disfruten mientras tú realizas tus obligaciones o te vas a tus vacaciones deseadas y no puedes llevarle contigo.",
-                "descriptionSecond": "Contamos con un lugar apropiado para poder tener a los perros como en casa, con espacio suficiente e instalaciones preparadas para todo tipo de perros (piscina incluida en verano).",
-                "slider": [
+            services: {
+                id: 1,
+                title: "Servicios",
+                descriptionFirst:
+                    "PerroBueno fue creado por amantes de los animales para darles un servicio personalizado. Es un lugar especial para que disfruten mientras tú realizas tus obligaciones o te vas a tus vacaciones deseadas y no puedes llevarle contigo.",
+                descriptionSecond:
+                    "Contamos con un lugar apropiado para poder tener a los perros como en casa, con espacio suficiente e instalaciones preparadas para todo tipo de perros (piscina incluida en verano).",
+                slider: [
                     {
-                        "id": 1,
-                        "alt": "perrobueno1",
-                        "image": 'tarifas'
+                        id: 1,
+                        alt: "perrobueno1",
+                        image: "tarifas",
                     },
                     {
-                        "id": 2,
-                        "alt": "perrobueno2",
-                        "image": 'perro_8'
+                        id: 2,
+                        alt: "perrobueno2",
+                        image: "perro_8",
                     },
                     {
-                        "id": 3,
-                        "alt": "perrobueno3",
-                        "image": 'educacion2'
+                        id: 3,
+                        alt: "perrobueno3",
+                        image: "educacion2",
                     },
                     {
-                        "id": 4,
-                        "alt": "perrobueno4",
-                        "image": 'perro_9'
-                    }
-                ]
+                        id: 4,
+                        alt: "perrobueno4",
+                        image: "perro_9",
+                    },
+                ],
             },
-            "homevets": {
-                "id": 1,
-                "logoname": "logoblanco",
-                "description": "Home vets by Bigvete SL con sus vehículos de atención a domicilio, totalmente equipados ofrece TODOS los servicios veterinarios para tu mascota.",
-                "videoUrl": "https://www.youtube.com/embed/ygBsGL4T_UU?rel=0&amp;showinfo=0"
+            homevets: {
+                id: 1,
+                logoname: "logoblanco",
+                description:
+                    "Home vets by Bigvete SL con sus vehículos de atención a domicilio, totalmente equipados ofrece TODOS los servicios veterinarios para tu mascota.",
+                videoUrl:
+                    "https://www.youtube.com/embed/ygBsGL4T_UU?rel=0&amp;showinfo=0",
             },
-            "showcase": [
+            showcase: [
                 {
-                    "id": 1,
-                    "secId": 'guarderia',
-                    "title": "Guardería",
-                    "description": "<p>Un miembro de nuestro equipo estará en todo momento con ellos para guiar los juegos, vigilar que todo vaya bien y que ningún perro sea excluido.</p> <p>Nos encargamos de controlar el estado vacunal de todos los perros que acudan a nuestro centro para asegurarnos de que todos están sanos y seguros. Hacemos traslado puerta a puerta cada mañana con una ruta fija desde primera hora ya estamos en ruta canina para que pasen la mayor cantidad de tiempo haciendo ejercicio.</p>",
-                    "image": ['guarderia'],
-                    "altImage": ['dogshowcase1']
+                    id: 1,
+                    secId: "guarderia",
+                    title: "Guardería",
+                    description:
+                        "<p>Un miembro de nuestro equipo estará en todo momento con ellos para guiar los juegos, vigilar que todo vaya bien y que ningún perro sea excluido.</p> <p>Nos encargamos de controlar el estado vacunal de todos los perros que acudan a nuestro centro para asegurarnos de que todos están sanos y seguros. Hacemos traslado puerta a puerta cada mañana con una ruta fija desde primera hora ya estamos en ruta canina para que pasen la mayor cantidad de tiempo haciendo ejercicio.</p>",
+                    image: ["guarderia"],
+                    altImage: ["dogshowcase1"],
                 },
                 {
-                    "id": 2,
-                    "secId": 'educacion',
-                    "title": "Educación",
-                    "description": "<p>Como todos nuestros servicios, además de ser personalizados, trabajamos desde el respeto a los animales.</p><p>Y en la educación, como en cualquier ser vivo es importante trabajar con respeto y paciencia. Damos soporte a las familias en todas las etapas de vuestros peludos, porque para ti es importante, para nosotros también.</p><p>Damos mucha importancia a la educación de cachorros, como en la ayuda a los perros de edad avanzada que tienen problemas de comportamiento, ya sea por malas experiencias o por consentimiento en casa.</p>",
-                    "descriptionHide": "<p>Perrobueno, en un principio siempre busca trabajar con las familias y las mascotas, de manera presencial en casa, barrio, parque o donde este el problema, para solucionarlo en equipo, y que las familias sepan cómo ayudar al peludo independientemente de la gravedad del problema del perro.</p><p>En casos más complicados, donde hay peligro en casa, para perros convivientes o miembros de la familia, no nos damos por vencidos, tenemos opciones más específicas y completas para trabajar con el peludo fuera del entorno familiar y del domicilio.</p><p>Ofrecemos un servicio de hospedaje y educación específica y una vez avanzado en el “problema” de comportamiento, hacemos un traspaso a las familias para encajar todas las piezas.</p><p><b>*Este servicio, de educación y alojamiento, sólo se da en casos de que nuestro profesional lo considere necesario, en todos los casos se realiza un diagnóstico previo, analizando la situación familiar y el estado del perro.</b></p><p>Siempre intentaremos encontrar la manera más asertiva y efectiva a la hora de trabajar con tu perro, no todos los perros necesitan la misma solución, ni tienen los mismos problemas, buscaremos la forma de estabilizar la convivencia en casa y en todos los entornos para que la vida con tu peludo sea lo que siempre has querido.</p>",
-                    "image": ['educacion1', 'worker'],
-                    "altImage": ['educacion1', 'educacion2']
+                    id: 2,
+                    secId: "educacion",
+                    title: "Educación",
+                    description:
+                        "<p>Como todos nuestros servicios, además de ser personalizados, trabajamos desde el respeto a los animales.</p><p>Y en la educación, como en cualquier ser vivo es importante trabajar con respeto y paciencia. Damos soporte a las familias en todas las etapas de vuestros peludos, porque para ti es importante, para nosotros también.</p><p>Damos mucha importancia a la educación de cachorros, como en la ayuda a los perros de edad avanzada que tienen problemas de comportamiento, ya sea por malas experiencias o por consentimiento en casa.</p>",
+                    descriptionHide:
+                        "<p>Perrobueno, en un principio siempre busca trabajar con las familias y las mascotas, de manera presencial en casa, barrio, parque o donde este el problema, para solucionarlo en equipo, y que las familias sepan cómo ayudar al peludo independientemente de la gravedad del problema del perro.</p><p>En casos más complicados, donde hay peligro en casa, para perros convivientes o miembros de la familia, no nos damos por vencidos, tenemos opciones más específicas y completas para trabajar con el peludo fuera del entorno familiar y del domicilio.</p><p>Ofrecemos un servicio de hospedaje y educación específica y una vez avanzado en el “problema” de comportamiento, hacemos un traspaso a las familias para encajar todas las piezas.</p><p><b>*Este servicio, de educación y alojamiento, sólo se da en casos de que nuestro profesional lo considere necesario, en todos los casos se realiza un diagnóstico previo, analizando la situación familiar y el estado del perro.</b></p><p>Siempre intentaremos encontrar la manera más asertiva y efectiva a la hora de trabajar con tu perro, no todos los perros necesitan la misma solución, ni tienen los mismos problemas, buscaremos la forma de estabilizar la convivencia en casa y en todos los entornos para que la vida con tu peludo sea lo que siempre has querido.</p>",
+                    image: ["educacion1"],
+                    altImage: ["educacion1"],
                 },
                 {
-                    "id": 3,
-                    "secId": 'peluqueria',
-                    "title": "Peluquería",
-                    "description": "<p>Nuestra peluquería canina <a href='https://www.instagram.com/perrobuenopetshop/?hl=es' target='_blank'>@perrobuenopetshop</a> trabaja con un sistema para minimizar el nivel de estrés de los perros y gatos en su momento de aseo.</p><h4>¿Qué es la peluquería de bajo Estrés?</h4><p>Es una peluquería donde prima el bienestar emocional del animal, para ello eliminamos o minimizamos todos los puntos estresantes de la sesión de peluquería. Por ello nos hemos formado en Educación canina en positivo y asistencia técnica veterinaria y realizamos las sesiones en un entorno amable. </p>",
-                    "descriptionHide": "<p>No se utilizan jaulas de espera, no se utilizan cabinas de secado, no hay más perros en la misma sala, no se utilizan correcciones ni castigos, se interpretan las señales que realiza el perro para saber hasta dónde podemos llegar en nuestros manipulados.</p><p>Hacemos uso de aromaterapia, musicoterapia, premios, juguetes, descansos, y el responsable del animal puede estar si así lo desea durante la sesión.</p><p>Todas las citas las gestionamos con cita previa para que tu peludo se sienta único como cuando tu vas a la peluquería. Para citas e información debes de comunicarte al <a href='tel:635222254'>635 22 22 54</a> y Patricia estará encantada de informarte de todo.</p><p>Nuestro centro está ubicado en Madrid, el Legazpi, en la calle paseo del molino 17.</p><p>Y nuestro Instagram <a target='_blank' href='https://www.instagram.com/perrobuenopetshop/?hl=es'>@perrobuenopetshop</a></p>",
-                    "image": ['pelu1'],
-                    "altImage": ['peluqueria']
-                }
+                    id: 3,
+                    secId: "peluqueria",
+                    title: "Peluquería",
+                    description:
+                        "<p>Nuestra peluquería canina <a href='https://www.instagram.com/perrobuenopetshop/?hl=es' target='_blank'>@perrobuenopetshop</a> trabaja con un sistema para minimizar el nivel de estrés de los perros y gatos en su momento de aseo.</p><h4>¿Qué es la peluquería de bajo Estrés?</h4><p>Es una peluquería donde prima el bienestar emocional del animal, para ello eliminamos o minimizamos todos los puntos estresantes de la sesión de peluquería. Por ello nos hemos formado en Educación canina en positivo y asistencia técnica veterinaria y realizamos las sesiones en un entorno amable. </p>",
+                    descriptionHide:
+                        "<p>No se utilizan jaulas de espera, no se utilizan cabinas de secado, no hay más perros en la misma sala, no se utilizan correcciones ni castigos, se interpretan las señales que realiza el perro para saber hasta dónde podemos llegar en nuestros manipulados.</p><p>Hacemos uso de aromaterapia, musicoterapia, premios, juguetes, descansos, y el responsable del animal puede estar si así lo desea durante la sesión.</p><p>Todas las citas las gestionamos con cita previa para que tu peludo se sienta único como cuando tu vas a la peluquería. Para citas e información debes de comunicarte al <a href='tel:635222254'>635 22 22 54</a> y Patricia estará encantada de informarte de todo.</p><p>Nuestro centro está ubicado en Madrid, el Legazpi, en la calle paseo del molino 17.</p><p>Y nuestro Instagram <a target='_blank' href='https://www.instagram.com/perrobuenopetshop/?hl=es'>@perrobuenopetshop</a></p>",
+                    image: ["pelu1"],
+                    altImage: ["peluqueria"],
+                },
             ],
-            "reviews": [
+            reviews: [
                 {
-                    "id": 1,
-                    "review": "Los mejores !! Mi perrita lleva 3 años yendo a Perrobueno y siempre vuelve a casa contentísima! Coty y su equipo cuidan los perros como son los suyos - 1000% recomendado!",
-                    "name": "Adrienne",
-                    "imageName": "Adrienne",
-                    "image": 'review1'
+                    id: 1,
+                    review: "Los mejores !! Mi perrita lleva 3 años yendo a Perrobueno y siempre vuelve a casa contentísima! Coty y su equipo cuidan los perros como son los suyos - 1000% recomendado!",
+                    name: "Adrienne",
+                    imageName: "Adrienne",
+                    image: "review1",
                 },
                 {
-                    "id": 2,
-                    "review": "Cala vuelve encantada de la guarde, no quiere bajarse de la furgo!!!. Muy cariñosos con los peludos, muy bien atendidos. Super puntuales con el horario de recogida. Muy recomendable.",
-                    "name": "Patricia",
-                    "imageName": "Patricia",
-                    "image": 'review2'
+                    id: 2,
+                    review: "Cala vuelve encantada de la guarde, no quiere bajarse de la furgo!!!. Muy cariñosos con los peludos, muy bien atendidos. Super puntuales con el horario de recogida. Muy recomendable.",
+                    name: "Patricia",
+                    imageName: "Patricia",
+                    image: "review2",
                 },
                 {
-                    "id": 3,
-                    "review": "Brida llega siempre feliz, feliz, feliz y agotada de jugar toda la mañana. El personal súper cariñoso y profesional!",
-                    "name": "Esther",
-                    "imageName": "Esther",
-                    "image": 'review3'
+                    id: 3,
+                    review: "Brida llega siempre feliz, feliz, feliz y agotada de jugar toda la mañana. El personal súper cariñoso y profesional!",
+                    name: "Esther",
+                    imageName: "Esther",
+                    image: "review3",
                 },
                 {
-                    "id": 4,
-                    "review": "Las mejores vacaciones de mis peques. El mejor sitio de Madrid,  te los recogen en la puerta de casa y te los entregan también en casa. Gracias equipo de Perrobueno",
-                    "name": "Monica",
-                    "imageName": "Monica",
-                    "image": 'review4'
+                    id: 4,
+                    review: "Las mejores vacaciones de mis peques. El mejor sitio de Madrid,  te los recogen en la puerta de casa y te los entregan también en casa. Gracias equipo de Perrobueno",
+                    name: "Monica",
+                    imageName: "Monica",
+                    image: "review4",
                 },
                 {
-                    "id": 5,
-                    "review": "Estoy súper encantada con el trato hacia nuestro Shadow y sobre todo con el trato que nos da y la cordialidad de Iván 👏🏼👏🏼👏🏼👏🏼",
-                    "name": "Marta",
-                    "imageName": "Marta",
-                    "image": 'review5'
+                    id: 5,
+                    review: "Estoy súper encantada con el trato hacia nuestro Shadow y sobre todo con el trato que nos da y la cordialidad de Iván 👏🏼👏🏼👏🏼👏🏼",
+                    name: "Marta",
+                    imageName: "Marta",
+                    image: "review5",
                 },
                 {
-                    "id": 6,
-                    "review": "Desde el primer día, Jojo llega encantado. Al segundo día ya ni se pensó si entrar en la jaula: se metió de un salto. Y a la vuelta se queda llorando mientras se va la furgoneta",
-                    "name": "Aida",
-                    "imageName": "Aida",
-                    "image": 'review6'
-                }
+                    id: 6,
+                    review: "Desde el primer día, Jojo llega encantado. Al segundo día ya ni se pensó si entrar en la jaula: se metió de un salto. Y a la vuelta se queda llorando mientras se va la furgoneta",
+                    name: "Aida",
+                    imageName: "Aida",
+                    image: "review6",
+                },
             ],
-            "holiday": {
-                "id": 1,
-                "logoName": "happyface",
-                "title": "¡Tu mascota de vacaciones!",
-                "description": "En PERROBUENO no hacemos distinción de razas, solo nos importa que tu perro sea feliz!"
+            holiday: {
+                id: 1,
+                logoName: "happyface",
+                title: "¡Tu mascota de vacaciones!",
+                description:
+                    "En PERROBUENO no hacemos distinción de razas, solo nos importa que tu perro sea feliz!",
             },
-            "teaming": {
-                "id": 1,
-                "logoName": "teaming2",
-                "title": "Únete a Nuestro Teaming",
-                "description": "Dona 1€ al mes a mi Grupo Teaming Esterilización a perros PPP"
+            teaming: {
+                id: 1,
+                logoName: "teaming2",
+                title: "Únete a Nuestro Teaming",
+                description:
+                    "Dona 1€ al mes a mi Grupo Teaming Esterilización a perros PPP",
             },
-            "contact": {
-                "id": 1,
-                "title": "Contacto",
-                "description": "<p>Todos los servicios de esta pagina web los puedes consultar por teléfono y concretar cita al <a href='tel: 627 74 19 47'>627 74 19 47</a> o también whatsapp.</p><p>No dejes a tu perro solo en casa cuando sales a trabajar, nosotros nos encargamos de él y lo recogemos en tu casa!</p>"
-            }
-        }
-    }
-}
+            contact: {
+                id: 1,
+                title: "Contacto",
+                description:
+                    "<p>Todos los servicios de esta pagina web los puedes consultar por teléfono y concretar cita al <a href='tel: 627 74 19 47'>627 74 19 47</a> o también whatsapp.</p><p>No dejes a tu perro solo en casa cuando sales a trabajar, nosotros nos encargamos de él y lo recogemos en tu casa!</p>",
+            },
+        },
+    },
+};
